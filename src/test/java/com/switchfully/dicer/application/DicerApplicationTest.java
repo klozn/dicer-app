26fe4948f3e27d434b14ec210c0d58a361e83ca5
Klaas Devits <klaas.devits@gmail.com>
2021-10-12 09:54:27 +0200

    Initial commit

---
+++ src/test/java/com/switchfully/dicer/application/DicerApplicationTest.java
@@ -0,0 +1,12 @@
+package com.switchfully.dicer.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DicerApplicationTest {
+    @Test
+    void justADummyTest() {
+        assertTrue(true); // this test will fail
+    }
+}
